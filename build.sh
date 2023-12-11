0d35d0f834aab16181694a98aa880caf509cbc31
jrmahapatra <mailme@mjanaki.com>
2023-12-10 22:52:08 -0700

    name chaged

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker image rm mjanaki/php-nginx-alpine
+docker image rm mjanaki/php-nginx-alpine-8.0
 
-docker build --tag mjanaki/php-nginx-alpine .
+docker build --tag mjanaki/php-nginx-alpine-8.0 .
 
 # docker push mjanaki/php-nginx-alpine:1.0
