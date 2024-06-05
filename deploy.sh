250009140d9b91ba7fe2622cc6d7239a575b3b91
jrmahapatra <mailme@mjanaki.com>
2024-06-05 07:43:27 -0700

    php8.0 updated with no sql server support

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ docker image rm mjanaki/php-nginx-alpine-8.0:1.0
 
 docker build --tag mjanaki/php-nginx-alpine-8.0:1.0 .
 
-# docker push mjanaki/php-nginx-alpine-8.0:1.0
+docker push mjanaki/php-nginx-alpine-8.0:1.0
