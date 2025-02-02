9166e7534091a6ba54f0b570bffd2dd593b787ba
jrmahapatra <mailme@mjanaki.com>
2025-02-02 14:53:37 -0700

    updated build

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,5 @@
-docker buildx build --platform linux/arm64,linux/amd64 -t mjanaki/php82-intl-alpine-nginx:latest mjanaki/php82-silicon-alpine-nginx . --push
+# Create a new builder instance with multi-platform support
+docker buildx create --name mybuilder --use
+
+# Build and push the multi-platform image
+docker buildx build --platform linux/arm64,linux/amd64 -t mjanaki/php82-alpine-nginx:latest . --push
